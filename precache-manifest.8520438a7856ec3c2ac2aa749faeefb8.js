ed426b4f33560c81b2b00e09c5b6b14160f384bc
Jermaine Lewis <lewisjermaine22@gmail.com>
2019-10-30 21:22:16 -0500

    Updates

---
+++ precache-manifest.8520438a7856ec3c2ac2aa749faeefb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30a07707bfa5b1be16e6e0698d0ce0a",
+    "url": "/TouchMemoryGame/index.html"
+  },
+  {
+    "revision": "d48b696f6f9705d88fb8",
+    "url": "/TouchMemoryGame/static/css/main.2c9ae118.chunk.css"
+  },
+  {
+    "revision": "d74e5daac1fffa8d0c47",
+    "url": "/TouchMemoryGame/static/js/2.bc7eeef8.chunk.js"
+  },
+  {
+    "revision": "d48b696f6f9705d88fb8",
+    "url": "/TouchMemoryGame/static/js/main.b2c61e3b.chunk.js"
+  },
+  {
+    "revision": "bc7c6433fa2d6358cf6e",
+    "url": "/TouchMemoryGame/static/js/runtime-main.124dd566.js"
+  },
+  {
+    "revision": "4b60a25ef5b5750b4afb7a32df068d17",
+    "url": "/TouchMemoryGame/static/media/SpaceStation.4b60a25e.jpg"
+  }
+]);
